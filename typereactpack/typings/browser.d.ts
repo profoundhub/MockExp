33819f05ab1dbe5e1a77baf0c5975ed2cfe928f9
Daniel Lim <github@profoundideation.com>
2016-04-01 23:44:17 -0400

    add testing framework, assertion and other plugins

--- typereactpack/typings/browser.d.ts
+++ typereactpack/typings/browser.d.ts
@@ -1,2 +1,5 @@
+/// <reference path="browser/ambient/chai/index.d.ts" />
+/// <reference path="browser/ambient/mocha/index.d.ts" />
 /// <reference path="browser/ambient/react-dom/index.d.ts" />
 /// <reference path="browser/ambient/react/index.d.ts" />
+/// <reference path="browser/ambient/sinon/index.d.ts" />
